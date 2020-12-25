22822636308658b23dd9478713c564e8fc85d3d3
tarun1754 <74248818+tarun1754@users.noreply.github.com>
2020-12-25 19:04:14 +0530

    Add files via upload

---
+++ division.js
@@ -0,0 +1,16 @@
+class Division {
+    constructor(x,y,width,height) {
+      var options = {
+          isStatic: true
+      }
+      this.body = Bodies.rectangle(x,y,width,height,options);
+      this.width = 50;
+      this.height = 10;
+      World.add(world, this.body);
+    }
+    display(){
+      var pos =this.body.position;
+      rectMode(CENTER);
+      rect(pos.x, pos.y, this.width, this.height);
+    }
+  };
